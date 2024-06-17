222f1cc55c662af340243012f8763ca86834260c
Justin <itinjus@gmail.com>
2024-06-17 08:50:37 +0800

    Updated the registration pages

--- login.js
+++ login.js
@@ -1,23 +1,24 @@
 console.log(localStorage);
 
+// Event listener for getting the form data
 document.addEventListener('DOMContentLoaded', function () {
-  // Get the forms
+  // Get the forms through the id
   const loginForm = document.getElementById('loginForm');
 
-  // Define the fields
+  // Define the fields that will be stored
   const loginFields = ['Username', 'Password'];
 
   // Function to get form data
   function getFormData(form, fields) {
     const data = {};
     for (let field of fields) {
-      const input = form.elements[field];
-      console.log(input);
+      const input = form.elements[field]; // Get form control named 'field' from 'form'
       if (!input || input.value === '') {
+        // testing for null value
         alert(`Please fill in the ${field}`);
         return null;
       }
-      data[field] = input.value;
+      data[field] = input.value; // Store the field in the data object
     }
     return data;
   }
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault();
     const data = getFormData(this, loginFields);
     if (data) {
-      // Check if the user is registered
+      // Check if the user is registered by accessing the localStorage
       const accountData = JSON.parse(localStorage.getItem('accountData'));
       if (
         accountData &&
--- register.js
+++ register.js
@@ -1,10 +1,11 @@
 'use strict';
 
+// Event listener for getting the form data
 document.addEventListener('DOMContentLoaded', function () {
-  // Get the forms
+  // Get the forms by selecting the id
   const registerForm = document.getElementById('registerForm');
 
-  // Define the fields
+  // Define the fields to be checked
   const registerFields = [
     'Username',
     'Password',
@@ -22,10 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let field of fields) {
       const input = form.elements[field];
       if (!input || input.value === '') {
+        // Checking for null
         alert(`Please fill in the ${field}`);
         return null;
       }
-      data[field] = input.value;
+      data[field] = input.value; // Stores the input value in the 'data' object with the field name as the key.
     }
     return data;
   }
@@ -33,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Register form submit event
   registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    const data = getFormData(this, registerFields);
+    const data = getFormData(this, registerFields); // Get the form data
     if (data) {
       localStorage.setItem('accountData', JSON.stringify(data));
       // Show the modal
